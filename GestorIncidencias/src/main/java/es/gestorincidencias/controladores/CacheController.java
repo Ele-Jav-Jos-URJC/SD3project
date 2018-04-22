3041f6ac572cb857b58dd4f2290268918e60f503
CAN <CAN@CAN-VAIO>
2018-04-22 20:52:02 +0200

    Incluyo clase para ver datos guardados en cache

---
+++ GestorIncidencias/src/main/java/es/gestorincidencias/controladores/CacheController.java
@@ -0,0 +1,29 @@
+package es.gestorincidencias.controladores;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CacheController{
+	
+	@Autowired
+	private CacheManager cacheManager;
+	
+	//Aplicacion para ver el contenido de la caché
+	@RequestMapping(value="/cache", method=RequestMethod.GET)
+	public Map<Object, Object> getCacheContent() {
+		ConcurrentMapCacheManager cacheMgr=(ConcurrentMapCacheManager) cacheManager;
+		ConcurrentMapCache cache=(ConcurrentMapCache) cacheMgr.getCache("Incidencia");
+		return cache.getNativeCache();
+		
+		
+	}
+}
+
